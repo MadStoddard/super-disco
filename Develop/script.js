095a144793b208db2fde763343f3383c6de84f1a
MadDAWG <[madelinejdierking@gmail.com]>
2022-03-26 11:09:47 -0400

    local storage save

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,41 @@
 var containerDiv = document.querySelector(".container");
+var descriptionEl = document.createElement("input");
+var currentDayEl = document.querySelector("#currentDay");
+currentDayEl.textContent = moment().format("dddd MMMM Do");
+
+// var auditTask = function(descriptionEl) {
+//     var date = $(descriptionEl).find("span").text().trim();
+//     var time = moment(date, "L").set("hour", 17);
+
+//     if(moment().isAfter(time)) {
+//         $(descriptionEl).addClass("past");
+//     } else if (Math.abs(moment().diff(time, "hours")) <= 1) {
+//         (descriptionEl).addClass("future")
+//     }
+// };
 
 var hourArray = [
     "9AM",
     "10AM",
     "11AM",
-    "12PM"
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM"
+]
+
+var basicTime = [
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17
 ]
 
 for (i = 0; i < hourArray.length; i++) {
@@ -17,21 +48,53 @@ for (i = 0; i < hourArray.length; i++) {
     timeBlockDiv.classList = "time-block row";
     hourH3.classList.add("hour");
     hourH3.textContent = hourArray[i]
-    descriptionEl.classList = "description";
+
+    if (basicTime[i] == moment().format("H")) {
+        descriptionEl.classList = "description present";
+    } else if (basicTime[i] < moment().format("H")) {
+        descriptionEl.classList = "description past";
+    } else {
+        descriptionEl.classList = "description future";
+    }
+
     descriptionEl.setAttribute("type","text");
+
+    if(localStorage.getItem(hourArray[i])) {
+        descriptionEl.value = localStorage.getItem(hourArray[i]);
+    } else {
+        descriptionEl.value = "";
+    }
+
+
+
     saveBtn.classList = "saveBtn";
     saveBtn.textContent = "Save";
+    saveBtn.setAttribute("data-time", basicTime[i])
     saveBtn.addEventListener("click", saveClick)
 
     containerDiv.append(timeBlockDiv);
     timeBlockDiv.append(hourH3);
     timeBlockDiv.append(descriptionEl);
     timeBlockDiv.append(saveBtn);
-}
+};
 
-function saveClick () {
-    alert("Hello")
-}
+function saveClick (event) {
+    // localStorage.setItem(descriptionEl, descriptionEl.value);
+
+    // get the value of the input text associated with the button that was clicked
+    var textToSave = event.target.previousElementSibling.value;
+
+    // get the time represented by the button that was clicked
+    var timeKey = event.target.previousElementSibling.previousElementSibling.textContent;
+    // var timeKey = event.target.getAttribute("data-time");
+
+    // save the text value to localstorage using the time as key
+    localStorage.setItem(timeKey, textToSave)
+    
+};
+
+
+
 
    
 
